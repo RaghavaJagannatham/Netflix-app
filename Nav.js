f3cd9fa7134994c8249a5918a6e9c7ae6187bb57
Raghava Jagannatham <70148332+RaghavaJagannatham@users.noreply.github.com>
2021-01-05 19:37:06 +0530

    Adding basic files
    committing some phenomenal changes for the Netflix-clone App according to the requirements

---
+++ Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img className="nav_logo"
+            src="https://www.freepnglogos.com/uploads/red-netflix-logo-text-png-3.png"
+            alt="Netflix Logo"/>
+
+            <img className="nav_avatar"
+            src="https://cdn4.iconfinder.com/data/icons/essential-set-4/64/menu-option-setting-preference-256.png"
+            alt="Nav_Avatar"/>  
+            
+        </div>
+    )
+}
+
+export default Nav
